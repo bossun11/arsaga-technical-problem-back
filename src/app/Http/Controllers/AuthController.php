0cf1f14a0db01421ad029b29dc0eb16082550528
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-01 20:40:24 +0900

    Add: ログイン機能を実装

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //ユーザー登録
+    // ユーザー登録
   public function register(Request $request) {
     $rules = [
       "name" => ["required", "string", "max:50"],
@@ -34,5 +35,30 @@ class AuthController extends Controller
         "data" => $user,
     ];
     return response()->json($json, Response::HTTP_OK);
+  }
+
+  // ログイン
+  public function login(Request $request) {
+    $rules = [
+      "email" => ["required", "string", "email", "max:255"],
+      "password" => ["required", "string", "min:8", "max:50"],
+    ];
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+    }
+
+    $data = $request->only(["email", "password"]);
+
+    if (Auth::attempt($data)) {
+      $user = User::where("email", $data["email"])->first();
+      $user->tokens()->delete();
+      $token = $user->createToken("login:user{$user->id}")->plainTextToken;
+
+      return response()->json(["token" => $token], Response::HTTP_OK);
+    }
+
+    return response()->json('認証に失敗しました', Response::HTTP_UNAUTHORIZED);
   }
 }
